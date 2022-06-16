67280b41e86b6e54a335f9c673c644014a5cd3bc
Robin Delbaere <hello@rdelbaere.fr>
2022-06-16 13:45:46 +0200

    FlagGuesser : load data

--- flagGuesser/app.js
+++ flagGuesser/app.js
@@ -1,2 +1,21 @@
+const countries = [];
 
+const init = () => {
+    const apiUrl = 'https://restcountries.com/v3.1/all';
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => prepareData(data));
+};
 
+const prepareData = data => {
+    for(let entry of data){
+        const country = {
+            name: entry.translations.fra.common,
+            code: entry.cca2,
+        };
+
+        countries.push(country);
+    }
+};
+
+init();
