4a41ccd20f1666fb2de5c29449f7c2edab099077
Robin Delbaere <hello@rdelbaere.fr>
2022-06-16 11:54:59 +0200

    postDisplay v1.2

--- postDisplay/app.js
+++ postDisplay/app.js
@@ -1,4 +1,5 @@
-const containerElement = document.getElementById('posts')
+const containerElement = document.getElementById('posts');
+const postTemplate = document.getElementById('postTemplate');
 const apiUrl = 'https://jsonplaceholder.typicode.com/posts';
 
 fetch(apiUrl)
@@ -21,15 +22,28 @@ const displayPosts = posts => {
 };
 
 const displayPost = post => {
-    const titleElement = document.createElement('h2');
+    const postElement = document.importNode(postTemplate.content, true);
+
+    const titleElement = postElement.querySelector('h2');
     titleElement.innerText = post.title;
 
-    const bodyElement = document.createElement('p');
+    const bodyElement = postElement.querySelector('p');
     bodyElement.innerText = post.body;
-
-    const postElement = document.createElement('div');
-    postElement.classList.add('post');
-    postElement.append(titleElement, bodyElement);
 
     containerElement.appendChild(postElement);
 };
+
+// Equivalent sans template
+// const displayPost = post => {
+//     const titleElement = document.createElement('h2');
+//     titleElement.innerText = post.title;
+//
+//     const bodyElement = document.createElement('p');
+//     bodyElement.innerText = post.body;
+//
+//     const postElement = document.createElement('div');
+//     postElement.classList.add('post');
+//     postElement.append(titleElement, bodyElement);
+//
+//     containerElement.appendChild(postElement);
+// };
