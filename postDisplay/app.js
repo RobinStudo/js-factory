4a94f92a2577e6be80f10d02b8f865ec73e2a6b5
Robin Delbaere <hello@rdelbaere.fr>
2022-06-16 11:35:36 +0200

    postDisplay v1.1

--- postDisplay/app.js
+++ postDisplay/app.js
@@ -1,21 +1,35 @@
 const containerElement = document.getElementById('posts')
 const apiUrl = 'https://jsonplaceholder.typicode.com/posts';
 
-const promise = fetch(apiUrl);
-promise.then(response => {
-    return response.json();
-}).then(posts => {
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(posts => displayPosts(posts));
+
+// Equivalent
+// const promise = fetch(apiUrl);
+// promise.then(response => {
+//     return response.json();
+// }).then(posts => {
+//     displayPosts(posts);
+// });
+
+
+const displayPosts = posts => {
     for(let post of posts){
-        const titleElement = document.createElement('h2');
-        titleElement.innerText = post.title;
-
-        const bodyElement = document.createElement('p');
-        bodyElement.innerText = post.body;
-
-        const postElement = document.createElement('div');
-        postElement.classList.add('post');
-        postElement.append(titleElement, bodyElement);
-
-        containerElement.appendChild(postElement);
+        displayPost(post);
     }
-});
+};
+
+const displayPost = post => {
+    const titleElement = document.createElement('h2');
+    titleElement.innerText = post.title;
+
+    const bodyElement = document.createElement('p');
+    bodyElement.innerText = post.body;
+
+    const postElement = document.createElement('div');
+    postElement.classList.add('post');
+    postElement.append(titleElement, bodyElement);
+
+    containerElement.appendChild(postElement);
+};
