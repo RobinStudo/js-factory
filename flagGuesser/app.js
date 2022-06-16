95bb10b399fb030a4175de9537546cf88d0ac08e
Robin Delbaere <hello@rdelbaere.fr>
2022-06-16 14:18:02 +0200

    Flag Guesser : start

--- flagGuesser/app.js
+++ flagGuesser/app.js
@@ -1,4 +1,10 @@
 const countries = [];
+const elements = {
+    startContainer: document.querySelector('.start-container'),
+    challengeContainer: document.querySelector('.challenge-container'),
+    loader: document.querySelector('.start-container .loader'),
+    startButton: document.querySelector('.start-container button'),
+};
 
 const init = () => {
     const apiUrl = 'https://restcountries.com/v3.1/all';
@@ -16,6 +22,24 @@ const prepareData = data => {
 
         countries.push(country);
     }
+
+    elements.loader.remove();
+    elements.startButton.classList.remove('hide');
+    elements.startButton.addEventListener('click', start, {
+        once: true,
+    });
+};
+
+const start = () => {
+    elements.startContainer.remove();
+    elements.challengeContainer.classList.remove('hide');
+};
+
+const nextChallenge = () => {
+    // Tirer 4 pays au hasard
+    // En choisir un comme bonne réponse
+    // Afficher son drapeau dans l'image
+    // Afficher les 4 propositions dans les boutons
 };
 
 init();
